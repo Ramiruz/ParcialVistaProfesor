c2bf83e491c56624ff81aff44b6f3e0adb5c2b37
Ramiruz <88344446+Ramiruz@users.noreply.github.com>
2022-04-23 23:55:55 -0500

    Add files via upload

---
+++ src/components/Proyecto.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+class Proyecto extends Component {
+    render() {
+        return ( 
+            <div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto #1</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p></p>
+        <a href="#" class="btn btn-dark">More...</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto #2</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p></p>
+        <a href="#" class="btn btn-dark">More...</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto #3</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p></p>
+        <a href="#" class="btn btn-dark">More...</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto #4</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p></p>
+        <a href="#" class="btn btn-dark">More...</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto #5</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p></p>
+        <a href="#" class="btn btn-dark">More...</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto #6</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p></p>
+        <a href="#" class="btn btn-dark">More...</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+            );
+        }
+    }
+    
+export default Proyecto;
