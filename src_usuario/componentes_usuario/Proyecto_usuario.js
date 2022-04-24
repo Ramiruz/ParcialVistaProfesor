8fff7e1aebb797c22be2b529ac8225d09aee0c4f
Ramiruz <88344446+Ramiruz@users.noreply.github.com>
2022-04-24 01:55:09 -0500

    Add files via upload

---
+++ src_usuario/componentes_usuario/Proyecto_usuario.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+class Proyecto_usuario extends Component {
+    render() {
+        return ( 
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto#1</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p><p>Progress: **%</p></p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto#2</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p><p>Progress: **%</p></p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto#3</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p><p>Progress: **%</p></p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto#4</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p><p>Progress: **%</p></p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto#5</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p><p>Progress: **%</p></p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Proyecto#6</h5>
+        <p class="card-text"><strong>Date: **/**/****</strong><p>Participants: *******</p><p>Progress: **%</p></p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+</div>
+
+            );
+        }
+    }
+    
+export default Proyecto_usuario;
