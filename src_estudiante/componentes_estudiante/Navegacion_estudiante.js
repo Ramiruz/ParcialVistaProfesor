7fb8ef92a104699b61678269c67ccc9b29f69705
Ramiruz <88344446+Ramiruz@users.noreply.github.com>
2022-04-24 06:01:08 -0500

    Create Navegacion_estudiante.js

---
+++ src_estudiante/componentes_estudiante/Navegacion_estudiante.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+class Navegacion_estudiante extends Component {
+    render() {
+        return (
+            <nav aria-label="Page navigation example">
+  <ul class="pagination justify-content-center" >
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+            );
+        }
+    }
+    
+export default Navegacion_estudiante;
